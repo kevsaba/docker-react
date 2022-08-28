c6e6cc42cf6776f7cae5e2966b4843ac45b611a1
kevin.sabatino <kevin.sabatino@maibornwolff.de>
2022-08-28 18:05:10 +0200

    testing another change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ahora si.
+          ahora si changed.
         </p>
         <a
           className="App-link"
